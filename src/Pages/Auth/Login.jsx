46cc297d9c5ac79f087da7c493f812ac2068d693
Hilal Ahmad <115874624+fullstack124@users.noreply.github.com>
2023-10-04 03:37:58 -0700

    Update Login.jsx
    fix:button in login page

--- src/Pages/Auth/Login.jsx
+++ src/Pages/Auth/Login.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link,useNavigate } from 'react-router-dom'
 
 const Login = () => {
+  const navigate=useNavigate();
+  const gotoDashboard=()=>{
+    navigate('/dashboard');
+  }
   return (
     <>
     <section class="bg-gray-50 dark:bg-gray-900">
@@ -23,7 +27,7 @@ const Login = () => {
                       <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
                   </div>
                   
-                  <Link to={'/dashboard'} class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</Link>
+                  <button onClick={gotoDashboard} class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</Link>
                   <p class="text-sm font-light text-gray-500 dark:text-gray-400">
                      Don't have account? <Link to="/" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Register here</Link>
                   </p>
